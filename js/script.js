cb3b6400ac25ad0e9bb7cbb803c3b14bca817707
Aaron-Z1016 <ninghaihaiz@gmail.com>
2024-11-29 01:54:26 +1100

    day1 update

--- js/script.js
+++ js/script.js
@@ -30,23 +30,54 @@ const monsterStats = document.querySelector('#monsterStats');
 const monsterNameText = document.querySelector('#MonsterNameText');
 const monsterHealthText = document.querySelector('#monsterHealthText');
 
+const weapons = [{name:'stick', power : 5},
+    {name: 'dagger', power: 30}, 
+    {name: 'claw hammer', power:50},
+    {name: 'sword', power: 100}];
+    
+// {} 是 对象字面量（Object Literal） 的语法，用于定义一个 JavaScript 对象。
+// 对象是键值对（key-value pairs）的集合，用于存储关联数据和更复杂的实体。
+const locations = [{
+    name:'town square', 'button text':['Go to store', 'Go to cave', 'Fight dragon'],
+    'button functions': [goStore, goCave, fightDragon],
+    //backslash让他知道这不是end of string 而是literally
+    text:'Your are in the town square. You see a sign that says \'Store.\''},{
+    name:'store', 'button text':['Buy 10 health (10 gold)', 'Buy weapon (30 gold)', 'Go to town square'],
+    'button functions': [buyHealth, buyWeapon, goTown],
+    text:'You enter the store.'},{
+    name:'cave', 'button text':['Fight slime', 'Fight fanged beast', 'Go to town square'],
+    'button functions': [fightSlime, fightBeast, goTown],
+    text:'You enter the cave. You see some monsters'
+
+    }];
+
 //initialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
+function update(location){
+    button1.innerText = location['button text'][0];
+    button2.innerText = location['button text'][1];
+    button3.innerText = location['button text'][2];
+    button1.onclick = location['button functions'][0];
+    button2.onclick = location['button functions'][1];
+    button3.onclick = location['button functions'][2];
+    text.innerText = location.text;
+
+}
+
+function goTown(){
+    update(locations[0]);
+}
+
 function goStore(){
-    button1.innerText = 'Buy 10 health (10 gold)';
-    button2.innerText = 'Buy weapon (30 gold)';
-    button3.innerText = 'Go to town square';
-    button1.onclick = buyHealth;
-    button2.onclick = buyWeapon;
-    button3.onclick = goTown;
-    text.innerText = 'You enter the store.'
+    update(locations[1]);
 }
 
 function goCave(){
-    console.log('Going to cave.')
+    // console.log('Going to cave.')
+    update(locations[2]);
 }
 
 function fightDragon(){
@@ -54,13 +85,36 @@ function fightDragon(){
 }
 
 function buyHealth(){
+    if(gold>=10){
+        gold -= 10;
+        health += 10;
+        goldText.innerText = gold;
+        healthText.innerText = health; 
+    } else{
+        text.innerText = 'You do not have enough gold to buy health.';
+    }
 
 }
 
 function buyWeapon(){
+    if (gold >= 30){
+        gold -= 30;
+        currentWeapon++;
+        goldText.innerText = gold;
+        let newWeapon = weapons[currentWeapon].name;
+        text.innerText = 'You now have a' + newWeapon + '.';
+        inventory.push(newWeapon);
+        text.innerText += 'In your inventory you have: ' + inventory;
+    } else{
+        text.innerText = 'You do not have enough gold to buy a weapon.'
+    }
 
 }
 
-function goTown(){
-    
+function fightBeast(){
+
+}
+
+function fightSlime(){
+
 }
